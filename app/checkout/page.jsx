42a4d49f5b2ce178eaaf36d39b0ab0c44544dd0b
KashanAdnan-Coder <computertechhub0@gmail.com>
2024-09-30 14:12:10 +0500

    stripe payment gateway added

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,47 +1,59 @@
 'use client'
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { removeSingleCart } from "@/redux/slice/cartSlice"
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
 
-const page = () => {
-    const cart = useSelector(state => state.cart)
-    const user = useSelector(state => state.user)
-    const dispatch = useDispatch()
-    let prices = []
-    const [pay, setPay] = useState("")
-    const payMoney = () => {
-        
-    }
-    cart.items.forEach(element => {
-        prices.push(element.price)
-    });
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+export default function PreviewPage() {
+    React.useEffect(() => {
+        // Check to see if this is a redirect back from Checkout
+        const query = new URLSearchParams(window.location.search);
+        if (query.get('success')) {
+            console.log('Order placed! You will receive an email confirmation.');
+        }
+
+        if (query.get('canceled')) {
+            console.log('Order canceled -- continue to shop around and checkout when you’re ready.');
+        }
+    }, []);
+
     return (
-        <div>
-            <h1 className='text-xl'>Checkout</h1>
-            <p>Review</p>
-            {
-                cart.items.map((item) => {
-                    return (
-                        <div className='flex items-center justify-start w-full px-4 py-6'>
-                            <img src={item.image} width={100} height={100} />
-                            <div className='ml-4'>
-                                <span> {item.title}</span>
-                                <span> ${item.price}</span>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-            <div>Payment Methods</div>
-            <select className='flex gap-4 mt-4' onChange={(e) => setPay(e.target.value)}>
-                <option>Select Payment Methods</option>
-                <option value={"stripe"}>Stripe</option>
-                <option value={"cash method"}>Cash Pay</option>
-                <option value={"easy paisa"}>Easy Pasia</option>
-            </select>
-            <button className='bg-[#455CE9] text-white px-4 py-3 rounded-md mt-4' onClick={pay}>Pay Now</button>
-        </div>
-    )
+        <form action="http://localhost:4000/api/checkout_sessions" method="POST">
+            <section>
+                <button type="submit" role="link">
+                    Checkout
+                </button>
+            </section>
+            <style jsx>
+                {`
+          section {
+            background: #ffffff;
+            display: flex;
+            flex-direction: column;
+            width: 400px;
+            height: 112px;
+            border-radius: 6px;
+            justify-content: space-between;
+          }
+          button {
+            height: 36px;
+            background: #556cd6;
+            border-radius: 4px;
+            color: white;
+            border: 0;
+            font-weight: 600;
+            cursor: pointer;
+            transition: all 0.2s ease;
+            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
+          }
+          button:hover {
+            opacity: 0.8;
+          }
+        `}
+            </style>
+        </form>
+    );
 }
-
-export default page
